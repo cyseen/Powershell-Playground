bc26ad03a173e800c423e06f99ef4d55b9576f88
cyseen <65134216+cyseen@users.noreply.github.com>
2020-11-08 11:03:35 +0100

    Add files via upload

---
+++ GetOutlookContacts.ps1
@@ -0,0 +1,4 @@
+﻿$Outlook=NEW-OBJECT –comobject Outlook.Application
+$Contacts=$Outlook.session.GetDefaultFolder(10).items
+$Contacts | Format-Table FullName,MobileTelephoneNumber,Email1Address
+
---
+++ ObfuscateExample.ps1
@@ -0,0 +1,24 @@
+﻿<#
+$psc = 'Write-Host "Test"'
+
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($psc)
+[Convert]::ToBase64String($bytes)
+
+
+
+
+$psc = "Vw4y1Gk1d14l1C01S14v1HM1d11g1CI1V14l1HM1d11i11=="
+#Invoke-Expression -Command ([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($psc.Replace("1", "A").Replace("4", "B"))))
+
+$bytes = [System.Text.Encoding]::Unicode.GetBytes('Invoke-Expression -Command ([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String("Vw4y1Gk1d14l1C01S14v1HM1d11g1CI1V14l1HM1d11i11==".Replace("1", "A").Replace("4", "B"))))')
+[Convert]::ToBase64String($bytes)
+#>
+
+
+$psc2 = "SQBuAHYAbwBrAGUALQBFAHgAcAByAGUAcwBzAGkAbwBuACAALQBDAG8AbQBtAGEAbgBkACAAKABbAFMAeQBzAHQAZQBtAC4AVABlAHgAdAAuAEUAbgBjAG8AZABpAG4AZwBdADoAOgBVAG4AaQBjAG8AZABlAC4ARwBlAHQAUwB0AHIAaQBuAGcAKABbAFM
+AeQBzAHQAZQBtAC4AQwBvAG4AdgBlAHIAdABdADoAOgBGAHIAbwBtAEIAYQBzAGUANgA0AFMAdAByAGkAbgBnACgAIgBWAHcANAB5ADEARwBrADEAZAAxADQAbAAxAEMAMAAxAFMAMQA0AHYAMQBIAE0AMQBkADEAMQBnADEAQwBJADEAVgAxADQAbAAxAE
+gATQAxAGQAMQAxAGkAMQAxAD0APQAiAC4AUgBlAHAAbABhAGMAZQAoACIAMQAiACwAIAAiAEEAIgApAC4AUgBlAHAAbABhAGMAZQAoACIANAAiACwAIAAiAEIAIgApACkAKQApAA=="
+
+powershell -enc $psc2
+
+
